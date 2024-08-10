d3d10942166d1f06c38e844374b7b5cdfdfc8308
Adeel <majeedadeel767@gmail.com>
2024-08-09 22:22:14 -0700

    reverse and sum digit fun added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 
 fn main(){
-    let n=9;
-    print!("fibo of a number {} is {}",n,is_prime(n));
+    // let n=9;
+    // print!("fibo of a number {} is {}",n,is_prime(n));
+    // let n=434343532;
+    // print!("sum of digit of number{} is{};",n,sum_of_digit(n));
+    let s="hello";
+    print!("reverse of string {} is : {}",s,reverse_digit(s));
 }
+
+// fn reverse_digit(s:&str)-> String{
+//     return s.chars().rev().collect()
+// }
+
+// fn sum_of_digit(n:u32)-> u32{
+//     return n.to_string().chars().map(|c|c.to_digit(10).unwrap()).sum();
+// }
 
 // fn is_prime(n:u32) -> bool {
 //     if n<=1 {
